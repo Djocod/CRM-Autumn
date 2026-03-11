c8ad0ff30d387989ce9637a57eb0a0fdce2dc582
Djocod <jordan.francoual@hotmail.com>
2026-03-11 12:07:03 +0100

    feat: added input for search with de name brand

--- server/controller/products.controller.js
+++ server/controller/products.controller.js
@@ -19,12 +19,12 @@ export async function handleGetProductsByBrand(req, res) {
       return res.status(400).json({ message: "Missing product Brand" });
     }
 
-    const product = await getProductByBrand(brand);
-    if (!product) {
+    const products = await getProductByBrand(brand);
+    if (!products || products.length === 0) {
       return res.status(404).json({ message: "Product not found !" });
     }
 
-    return res.status(200).json(product);
+    return res.status(200).json({ products });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
